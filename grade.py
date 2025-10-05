257d47da49e8962745eb823a7f0778b656c72bed
Robert Wasinger <robwaz@robwaz.net>
2025-10-04 21:47:18 -0700

    afhadjsklg

--- grade.py
+++ grade.py
@@ -1,6 +1,6 @@
 def grade(data):
     import js
     js.document.title = 'New window title'
-    return {"overall": dict(credit=1, letter="a"),
+    return {"overall": dict(credit=1, letter="<script>alert('hello world')</script>a"),
             "assignments": []
             }
